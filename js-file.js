24af7487a335c7dbb77e345329d84fbcf02972e2
Matthew McAuley <mattwilliam618@gmail.com>
2023-11-09 21:12:29 -0500

    Created operator function
    Needed function to call other arithmetic function based on operator
    provided

--- js-file.js
+++ js-file.js
@@ -13,3 +13,20 @@ function multiplty(num1, num2) {
 function divide(num1, num2) {
     return num1/num2;
 }
+
+function operate(num1, num2, operator) {
+    if (operator == "+") {
+        return add(num1, num2);
+    }
+    if (operator == "-") {
+        return subtract(num1, num2);
+    }
+    if (operator == "*") {
+        return multiply(num1, num2);
+    }
+    if (operator == "/") {
+        return divide(num1, num2);
+    }
+}
+
+let num1, operator, num2;
